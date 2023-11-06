da787d358e4c71d15a53f5472ebc833ab0b71e7a
USER <USER@tmhta>
2023-11-07 00:26:58 +0800

    viva question 2

---
+++ src/viva1/viva2.java
@@ -0,0 +1,23 @@
+package viva1;
+
+import java.util.Scanner;
+
+public class viva2 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter any Integer : ");
+        int num = scn.nextInt();
+        System.out.println("Reversed Integer : ");
+        while (true) {
+            if (num != 0)
+            {
+                int balance = num % 10;
+                System.out.print(balance);
+                num /= 10;
+            }
+            else 
+                break;
+        }
+        System.out.println(" ");
+    }
+}
