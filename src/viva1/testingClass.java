eb3a0bb2361b050b37c578f7da3717916e5f0531
haziqhalim <haziqhalim@localhost>
2023-11-06 23:51:34 +0800

    new testing class file (for demo purposes)

---
+++ src/viva1/testingClass.java
@@ -0,0 +1,13 @@
+package viva1;
+
+
+public class testingClass {
+    
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello World!");
+        
+    }
+    
+}
