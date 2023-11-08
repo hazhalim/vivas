943367f9bfb1329e1401d085713b433628932236
wertyg775 <150067831+wertyg775@users.noreply.github.com>
2023-11-08 16:19:53 +0800

    Add files via upload

---
+++ src/viva1/vivaQ1ver2.java
@@ -0,0 +1,25 @@
+
+public class vivaQ1ver2{
+public static void main(String args[]){
+
+
+int rows= 5;
+
+System.out.println("");
+for (int i=1; i<=rows; i++){ //outer loop
+  for (int j=1; j<=(rows-i); j++){ 
+  System.out.print(" "); // creating space for pyramid shape
+  }
+  
+for (int k=i; k>=1; k--){ //inner for loop
+  System.out.print(""+k);  // creating left pyramid 
+  
+}      
+
+for (int l=2; l<=i; l++){ 
+  System.out.print(""+l); //creating right pyramid   
+}                            
+System.out.println();
+}                                                 
+}
+}
